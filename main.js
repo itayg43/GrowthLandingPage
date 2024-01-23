0271b7e21a277b6789920d5a7e0b23851febd1c5
itayg43 <93944494+itayg43@users.noreply.github.com>
2024-01-24 01:07:01 +0200

    edit: separate logic into different functions

--- main.js
+++ main.js
@@ -1,47 +1,50 @@
-// faq
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", main);
+
+function main() {
+  const hamburgerButton = document.querySelector(".hamburger-button");
   const faqContainer = document.querySelector(".faq-content");
 
-  faqContainer.addEventListener("click", (e) => {
-    const faqGroupHeader = e.target.closest(".faq-group-header");
+  hamburgerButton.addEventListener("click", handleMobileMenuPress);
+  faqContainer.addEventListener("click", (e) =>
+    handleFaqContainerPress(e, faqContainer)
+  );
+}
 
-    if (faqGroupHeader === null) return;
-
-    const faqGroupHeaderIcon = faqGroupHeader.querySelector("i");
-
-    const faqGroup = faqGroupHeader.parentElement;
-    const faqGroupBody = faqGroup.querySelector(".faq-group-body");
-
-    // toggle icons
-    faqGroupHeaderIcon.classList.toggle("fa-plus");
-    faqGroupHeaderIcon.classList.toggle("fa-minus");
-
-    // toggle body open status
-    faqGroupBody.classList.toggle("open");
-
-    const allFaqGroups = faqContainer.querySelectorAll(".faq-group");
-
-    // close other open faq body and toggle icon
-    allFaqGroups.forEach((currFaqGroup) => {
-      if (currFaqGroup !== faqGroup) {
-        const icon = currFaqGroup.querySelector(".faq-group-header i");
-        const body = currFaqGroup.querySelector(".faq-group-body");
-
-        icon.classList.remove("fa-minus");
-        icon.classList.add("fa-plus");
-
-        body.classList.remove("open");
-      }
-    });
-  });
-});
-
-// mobile menu
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburgerButton = document.querySelector(".hamburger-button");
+function handleMobileMenuPress() {
   const mobileMenu = document.querySelector(".mobile-menu");
 
-  hamburgerButton.addEventListener("click", () => {
-    mobileMenu.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+}
+
+function handleFaqContainerPress(e, faqContainerRef) {
+  const faqGroupHeader = e.target.closest(".faq-group-header");
+
+  if (faqGroupHeader === null) return;
+
+  const faqGroupHeaderIcon = faqGroupHeader.querySelector("i");
+
+  const faqGroup = faqGroupHeader.parentElement;
+  const faqGroupBody = faqGroup.querySelector(".faq-group-body");
+
+  // toggle icons
+  faqGroupHeaderIcon.classList.toggle("fa-plus");
+  faqGroupHeaderIcon.classList.toggle("fa-minus");
+
+  // toggle body open status
+  faqGroupBody.classList.toggle("open");
+
+  const allFaqGroups = faqContainerRef.querySelectorAll(".faq-group");
+
+  // close other open faq body and toggle their icon
+  allFaqGroups.forEach((currFaqGroup) => {
+    if (currFaqGroup !== faqGroup) {
+      const icon = currFaqGroup.querySelector(".faq-group-header i");
+      const body = currFaqGroup.querySelector(".faq-group-body");
+
+      icon.classList.remove("fa-minus");
+      icon.classList.add("fa-plus");
+
+      body.classList.remove("open");
+    }
   });
-});
+}
